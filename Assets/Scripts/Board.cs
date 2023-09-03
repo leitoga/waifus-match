e0f078029ca2cdc6cd33566d56f0443348b9cb87
leitoga <32745189+leitoga@users.noreply.github.com>
2023-09-03 12:12:59 -0300

    movimientos

--- Assets/Scripts/Board.cs
+++ Assets/Scripts/Board.cs
@@ -1,6 +1,7 @@
 using System;
 using System.Collections;
 using System.Collections.Generic;
+using UnityEditor;
 using UnityEngine;
 
 public class Board : MonoBehaviour
@@ -12,11 +13,40 @@ public class Board : MonoBehaviour
     public float cameraSizeOffset;
     public float cameraVerticalOffset;
 
+    public GameObject[] availablePiece;
+
+    Tile[,] Tiles;
+    Piece[,] Pieces;
+
+    Tile startTile;
+    Tile endTile;
+    private Tile tile_;
+
+
     // Start is called before the first frame update
     void Start()
     {
+        Tiles = new Tile[width, height];
+        Pieces = new Piece[width, height];
+
         SetupBoard();
         PositionCamer();
+        SetupPieces();
+    }
+
+    private void SetupPieces()
+    {
+        for (int x = 0; x < width; x++)
+        {
+            for (int y = 0; y < height; y++)
+            {
+                var selectedPiece = availablePiece[UnityEngine.Random.Range(0, availablePiece.Length)];
+                var o = Instantiate(selectedPiece, new Vector3(x, y, -5), Quaternion.identity);
+                o.transform.parent = transform;
+                Pieces[x, y] = o.GetComponent<Piece>();
+                Pieces[x, y].Setup(x, y, this);
+            }
+        }
     }
 
     private void PositionCamer()
@@ -40,13 +70,41 @@ public class Board : MonoBehaviour
             {
                 var o = Instantiate(tileObject, new Vector3(x, y, -5), Quaternion.identity);
                 o.transform.parent = transform;
+                Tiles[x, y] = o.GetComponent<Tile>();
+                Tiles[x, y]?.Setup(x, y, this);
             }
         }
     }
 
-    // Update is called once per frame
-    void Update()
+    public void TileDown(Tile tile_)
     {
-        
+        startTile = tile_;
+    }
+
+    public void TileOver(Tile tile_)
+    {
+        endTile = tile_;
+    }
+
+    public void TileUp(Tile tile_)
+    {
+        if(startTile!=null && endTile != null)
+        {
+            SwapTiles();
+        }
+        startTile = null;
+        endTile = null;
+    }
+
+    private void SwapTiles()
+    {
+        var StarPiece = Pieces[startTile.x, startTile.y];
+        var EndPiece = Pieces[endTile.x, endTile.y];
+
+        StarPiece.Move(endTile.x, endTile.y);
+        EndPiece.Move(startTile.x, startTile.y);
+
+        Pieces[startTile.x, startTile.y] = EndPiece;
+        Pieces[endTile.x, endTile.y] = StarPiece;
     }
 }
